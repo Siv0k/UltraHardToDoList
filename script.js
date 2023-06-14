3b1413833f0eb68530bf1344783284a22d071899
Siv0k <artvlad2003@gmail.com>
2023-06-15 00:13:47 +0300

    upgraded js

--- script.js
+++ script.js
@@ -1,94 +1,94 @@
 function addTask() {
-    const list = document.getElementsByClassName('list')[0];
     const input = document.getElementById("input").value;
-    const p = document.getElementById("now");
-    const ul = document.querySelector("ul");
-    const li = document.createElement("li");
-    const checkbox = document.createElement("input");
-    const button = document.createElement("button");
-    const nowLi = document.querySelectorAll("li");
-    const span = document.createElement("span");
-  
-
-    button.textContent = "X";
-    button.id = "delete";
-    checkbox.type = "checkbox";
-    checkbox.id = "check";
-    span.textContent = input;
-    let exit = false;
 
     if (input === "") {
         alert("Вы не ввели задачу, попробуйте еще раз");
         return;
     }
     
-    nowLi.forEach( (li) => {
-        const taskTekst = li.querySelector("span").textContent;
-        if (taskTekst == input) {
-            alert("Такая задача уже есть");
-            exit = true;
-            return;
-        }
-    });
+    const repitTask = Array.from(document.querySelectorAll("li span")).some((span) => span.textContent === input);
 
-    if (exit) {
+    if (repitTask) {
+        alert("Такая задача уже есть, попробуйте еще раз");
         return;
     }
-
-    if (!ul) {
-        p.remove();
-        const newUl = document.createElement("ul");
-        newUl.id = "nowUl";
-        list.appendChild(newUl);
-        newUl.appendChild(li);
-        li.appendChild(span);
-        li.appendChild(checkbox);
-        li.appendChild(button);
-    } else {
-        ul.appendChild(li); 
-        li.appendChild(span);
-        li.appendChild(checkbox);
-        li.appendChild(button);
-    }
-}
     
+    const li = document.createElement("li");
+    const checkbox = document.createElement("input");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    
+    button.textContent = "X";
+    button.id = "delete";
+    checkbox.type = "checkbox";
+    checkbox.id = "check";
+    span.textContent = input;
 
-document.getElementById("add").addEventListener("click", addTask);
+    li.appendChild(span);
+    li.appendChild(checkbox);
+    li.appendChild(button);
 
-document.addEventListener("click", function(e) {
+    const ul = document.getElementById("nowUl") || createNewList();
+    ul.appendChild(li);
+}
+
+function createNewList() {
     const list = document.getElementsByClassName('list')[0];
+    const p = document.getElementById("now");
+    const ul = document.createElement("ul");
+    ul.id = "nowUl";
+    list.appendChild(ul);
+    p?.remove();
+    return ul;
+}
+
+function removeTask() {
+    this.parentElement.remove();
+    const list = document.getElementsByClassName('list')[0];
+    const doneList = document.getElementsByClassName('doneList')[0];
     const li = list.querySelectorAll("li");
     const p = document.getElementById("now");
-    if (e.target.id === "delete") {
-        e.target.parentElement.remove();
-    }
+
     if (li.length === 0 && !p) {
-        const p = document.createElement("p");
-        p.textContent = "Нет задач";
-        p.id = "now";
-        list.appendChild(p);
+        const newP = document.createElement("p");
+        newP.textContent = "Нет задач";
+        newP.id = "now";
+        list.appendChild(newP);
     }
-});
+}
 
-
-document.addEventListener("click", function(e) {
+function checkTask() {
     const doneList = document.getElementsByClassName("doneList")[0];
     const ul = doneList.querySelector("ul");
-    p = doneList.querySelector("p");
-    if (e.target.id === "check") {
-        if (e.target.checked) {
-            if (!ul) {
-            p.remove();
+    const p = doneList.querySelector("p");
+    const li = this.parentElement;
+
+    if (this.checked) {
+        if (!ul) {
+            p?.remove();
             const newUl = document.createElement("ul");
             newUl.id = "doneUl";
             doneList.appendChild(newUl);
-            newUl.appendChild(e.target.parentElement);
-            } else {  
-            ul.appendChild(e.target.parentElement);
-            }
+            newUl.appendChild(li);
         } else {
-            const ul = document.getElementById('nowUl');
-            ul.appendChild(e.target.parentElement);
+            ul.appendChild(li);
         }
+    } else {
+        const ul = document.getElementById('nowUl');
+        ul.appendChild(li);
+    }
+}
+
+document.getElementById("add").addEventListener("click", addTask);
+
+document.addEventListener("click", (e) => { 
+    if (e.target.id === "delete") {
+        removeTask.call(e.target);
+    }
+});
+
+document.addEventListener("click", (e) => { 
+    if (e.target.id === "check") {
+        checkTask.call(e.target);
     }
 });
